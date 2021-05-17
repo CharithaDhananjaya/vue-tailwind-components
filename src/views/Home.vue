51d8094dac393d57e623a7406f51f4c27f90ff76
Charitha Dhananjaya <charitha.dhananjaya464@gmail.com>
2021-05-17 15:09:46 +0530

    Initial Commit.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App * Developed by Charitha Dhananjaya *" />
   </div>
 </template>
 
