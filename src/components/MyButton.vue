fdafa03ffebc12d5ee955f2f235653fd593b7b56
Charitha Dhananjaya <charitha.dhananjaya464@gmail.com>
2021-05-20 23:23:55 +0530

    Component rebuild.

---
+++ src/components/MyButton.vue
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+<template>
+  <button
+    type="button"
+    class="bg-blue-600 hover:bg-gray-100 py-2 px-4 rounded border border-gray-100"
+  >
+    Charitha
+  </button>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+};
+</script>
---
+++ src/views/Cards.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container mx-auto">
+    <div class="grid grid-cols-3 p-4 gap-4">
+      <div class="grid grid-cols-2 gap-4">
+        <MyButton class="row-span-2"></MyButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyButton from "../components/MyButton";
+
+export default {
+  name: "Home",
+  components: {
+    MyButton,
+  },
+};
+</script>
