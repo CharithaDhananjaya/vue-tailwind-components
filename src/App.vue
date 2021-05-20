8ba09a1cfb10e35b29b2d46ae701f9b09e92acdf
Charitha Dhananjaya <charitha.dhananjaya464@gmail.com>
2021-05-20 23:23:40 +0530

    Nav bar added.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
+  <div class="container mx-auto">
+    <div class="flex flex-row-reverse py-2 gap-4">
+      <a href="/Cards">
+        <span class="font-sans text-lg font-semibold text-green-700"
+          >Cards</span
+        >
+      </a>
+      <a href="/">
+        <span class="font-sans text-lg font-semibold text-green-700">Home</span>
+      </a>
+    </div>
+  </div>
   <router-view />
 </template>
